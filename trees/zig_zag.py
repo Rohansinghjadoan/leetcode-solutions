55eb799a03400cd8aeb92cb5e20ece5022763d90
rohan <rohansinghjadoan@gmail.com>
2025-07-31 23:06:21 +0530

    day 9

---
+++ trees/zig_zag.py
@@ -0,0 +1,26 @@
+from collections import deque
+class Solution(object):
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[List[int]]
+        """
+        if not root :
+            return []
+        result=[]
+        queue=deque([root])
+        is_left_to_right=True
+        while queue:
+            level_size=len(queue)
+            current_level=[0]*level_size
+            for i in range(level_size):
+                node=queue.popleft()
+                index=i if is_left_to_right else level_size-1-i
+                current_level[index]=node.val
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(current_level)
+            is_left_to_right= not is_left_to_right
+        return result
